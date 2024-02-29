05988ffad922ebed14df7e259dfd6d64cf3d36d8
TheSmallTeaBoi <theoiturri@tutanota.com>
2024-02-29 10:23:58 -0300

    fix polybar, add wallpaper

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -3,5 +3,10 @@
   home.username = "theo";
   home.homeDirectory = "/home/theo";  
   home.stateVersion = "23.11";
+
+  home.file."/home/theo/Documents/wallpapers/solidcolor.png" = {
+    enable = true; # disable this if you want to add your own wallpaper
+    source = ./wallpaper.png; # or change this
+  };
 }
 
