c66b63dccf59e38860e2a2be9d2bf41ba880e476
TheSmallTeaBoi <theoiturri@tutanota.com>
2025-11-11 01:23:29 -0300

    Add hardware acceleration for firefox, in theory.

--- modules/system/GUI-nvidia.nix
+++ modules/system/GUI-nvidia.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   config,
+  lib,
   pkgs,
   ...
 }:
@@ -25,6 +26,30 @@
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
+
+  # Firefox hardware acceleration.
+  environment.variables.LIBVA_DRIVER_NAME = "nvidia";
+  environment.variables.MOZ_DISABLE_RDD_SANDBOX = "1";
+
+  programs.firefox.preferences =
+    let
+      ffVersion = config.programs.firefox.package.version;
+    in
+    {
+      "media.ffmpeg.vaapi.enabled" = lib.versionOlder ffVersion "137.0.0";
+      "media.hardware-video-decoding.force-enabled" = lib.versionAtLeast ffVersion "137.0.0";
+      "media.rdd-ffmpeg.enabled" = lib.versionOlder ffVersion "97.0.0";
+
+      "gfx.x11-egl.force-enabled" = true;
+      "widget.dmabuf.force-enabled" = true;
+
+      # Set this to true if your GPU supports AV1.
+      #
+      # This can be determined by reading the output of the
+      # `vainfo` command, after the driver is enabled with
+      # the environment variable.
+      "media.av1.enabled" = false;
+    };
 
   programs.hyprland = {
     enable = true;
