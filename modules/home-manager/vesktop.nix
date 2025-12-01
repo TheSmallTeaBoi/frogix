267a3eadab922d17d450bc9e6fce31da8a408797
TheSmallTeaBoi <theoiturri@tutanota.com>
2025-12-01 00:55:44 -0300

    change vesktop config

--- modules/home-manager/vesktop.nix
+++ modules/home-manager/vesktop.nix
@@ -11,26 +11,30 @@
       arRPC = true;
       enabledThemes = [ "stylix.css custom.css" ];
     };
+    vencord = {
+      themes = {
+        custom =
+          # css
+          ''
+            /* /* Dubyas list of annoying stuff he hides in discord using Quick CSS */ */
+            /* @import url("https://raw.githubusercontent.com/DubyaDude/DubyasCleanupOfDiscord/main/DubyasCleanupOfDiscord.css"); */
+
+            /* Disables the dumb top bar */
+            div [class="bar_c38106"]
+            {
+              display: none;
+            }
+            :root
+            {
+              --custom-app-top-bar-height: 0px;
+            }
+
+            /* Add 12px offset to the server list, fixes the uncomfortable look when removing the titlebar*/
+            nav.wrapper_ef3116 {
+              padding-top: 12px;
+            }
+          '';
+      };
+    };
   };
-  xdg.configFile."vesktop/themes/custom.css".text =
-    # css
-    ''
-      /* Dubyas list of annoying stuff he hides in discord using Quick CSS */
-      @import url("https://raw.githubusercontent.com/DubyaDude/DubyasCleanupOfDiscord/main/DubyasCleanupOfDiscord.css");
-
-      /* Disables the dumb top bar */
-      div [class="bar_c38106"]
-      {
-        display: none;
-      }
-      :root
-      {
-        --custom-app-top-bar-height: 0px;
-      }
-
-      /* Add 12px offset to the server list, fixes the uncomfortable look when removing the titlebar*/
-      nav.wrapper_ef3116 {
-        padding-top: 12px;
-      }
-    '';
 }
