19538945dc981ed21a559ea30a8ad0b05912d7bc
TheSmallTeaBoi <theoiturri@tutanota.com>
2026-01-08 15:56:37 -0300

    remove modified steam desktop entry

--- modules/home-manager/desktop.nix
+++ modules/home-manager/desktop.nix
@@ -1,14 +1,14 @@
 { ... }:
 {
   xdg.desktopEntries = {
-    steam = {
-      name = "Steam";
-      genericName = "Game Store";
-      exec = "steam -no-cef-sandbox";
-      icon = "steam";
-      terminal = false;
-      categories = [ "Application" ];
-    };
+    # steam = {
+    #   name = "Steam";
+    #   genericName = "Game Store";
+    #   exec = "steam -no-cef-sandbox";
+    #   icon = "steam";
+    #   terminal = false;
+    #   categories = [ "Application" ];
+    # };
     vesktop = {
       name = "Vesktop";
       exec = "vesktop --enable-features=VaapiIgnoreDriverChecks,VaapiVideoEncoder,VaapiVideoDecoder,UseMultiPlaneFormatForHardwareVideo";
